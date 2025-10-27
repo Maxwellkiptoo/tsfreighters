bbed88186ad934d1b65c7460ede7767034e1dd32
Maxwell Techguy <kiptoomaxwell82@gmail.com>
2025-10-27 13:44:51 +0300

    checking the login issues,

--- config/config.php
+++ config/config.php
@@ -6,3 +6,15 @@ return [
     'pass' => '',
     'charset' => 'utf8mb4',
 ];
+
+// if (!defined('BASE_PATH')) {
+//     define('BASE_PATH', __DIR__ . '/../../');
+// }
+// if (!defined('BASE_URL')) {
+//     $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443)
+//         ? "https://" : "http://";
+//     $host = $_SERVER['HTTP_HOST'];
+//     $folder = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
+//     define('BASE_URL', $protocol . $host . $folder . '/');
+// }
+
